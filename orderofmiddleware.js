695e5cfe121484f5ae8ca32f8a4ae700edbad607
sabarinathanv64 <60459542+sabarinathanv64@users.noreply.github.com>
2020-02-11 18:12:53 +0530

    Create orderofmiddleware.js

---
+++ orderofmiddleware.js
@@ -0,0 +1,20 @@
+try{
+var express = require('express');
+var app = express();
+app.use(function(req, res, next){
+   console.log("Start");
+   next();
+});
+app.get('/', function(req, res,next){
+   res.send("Middle");
+   next();
+});
+
+app.use('/', function(req, res){
+   console.log('End');
+});
+
+app.listen(3000);}
+catch(ee){
+console.log(ee.message)
+}
